804cba224b34c1d189d1c6010e74f5b35c4b9de4
Kevin Lyons <kevin.m.lyons@pm.me>
2025-02-10 13:59:47 -0500

    implement api helpers and word count logic

--- backend/ecfr_api_utils.py
+++ backend/ecfr_api_utils.py
@@ -1,7 +1,10 @@
-from dataclasses import dataclass
-from typing import List, Dict, Union
+import re
+from dataclasses import dataclass, asdict
+from typing import List, Dict, Any, Optional
+from urllib.parse import urljoin
 
 import requests
+from lxml import etree
 
 
 BASE_URL = "https://www.ecfr.gov/"
@@ -10,7 +13,38 @@ BASE_URL = "https://www.ecfr.gov/"
 @dataclass
 class CfrReference:
     title: int
-    chapter: str  # NB: can be Arabic or Roman numeral (seems not to matter, just request whichever is provided)
+    chapter: Optional[
+        str
+    ]  # NB: can be Arabic or Roman numeral (seems not to matter, just request whichever is provided)
+    subtitle: Optional[str]
+    subchapter: Optional[str]
+    part: Optional[str]
+    subpart: Optional[str]
+    section: Optional[str]
+    appendix: Optional[str]
+
+    @classmethod
+    def from_json(cls, json_list: List[Dict[str, Any]]) -> List["CfrReference"]:
+        result = []
+        for d in json_list:
+            result.append(
+                cls(
+                    title=d["title"],
+                    chapter=d.get("chapter"),
+                    subtitle=d.get("subtitle"),
+                    subchapter=d.get("subchapter"),
+                    part=d.get("part"),
+                    subpart=d.get("subpart"),
+                    section=d.get("section"),
+                    appendix=d.get("appendix"),
+                )
+            )
+        return result
+
+    @property
+    def query_params(self) -> Dict[str, Any]:
+        d = asdict(self)
+        return {k: v for k, v in d.items() if v is not None}
 
 
 @dataclass
@@ -20,23 +54,58 @@ class Agency:
     cfr_references: List[CfrReference]
 
     @classmethod
-    def from_json(cls, json_response: Dict[str, Union[str, int]]) -> "Agency":
-        raise NotImplementedError
+    def from_json(cls, json_response: Dict[str, Any]) -> "Agency":
+        return cls(
+            name=json_response["name"],
+            short_name=json_response["short_name"],
+            cfr_references=CfrReference.from_json(json_response["cfr_references"]),
+        )
 
 
 @dataclass
-class Chapter:
+class Regulation:
     paragraphs: List[str]
     # other metadata
 
     @classmethod
-    def from_response(cls, response: requests.Response) -> "Chapter":
-        raise NotImplementedError
+    def from_response(cls, response: requests.Response) -> "Regulation":
+        """
+        Currently dumb approach to get all text in <P></P> xml tags in
+        a given chapter, not yet robust to differences in tags between
+        different titles/chapters/etc.
+
+        Also currently ignores headings and other potentially useful
+        metadata
+        """
+        root = etree.fromstring(response.content)
+        paragraph_tags = root.xpath("//P")
+        paragraph_texts = []
+        for paragraph_tag in paragraph_tags:
+            text = paragraph_tag.text
+            if text:
+                text = text.strip()
+                text = re.sub(r"\s+", " ", text)
+                if text:  # skip empty strings
+                    paragraph_texts.append(text)
+        return cls(paragraphs=paragraph_texts)
 
 
 def get_agencies() -> List[Agency]:
-    raise NotImplementedError
+    agencies_endpoint = "/api/admin/v1/agencies.json"
+    url = urljoin(BASE_URL, agencies_endpoint)
+    r = requests.get(url)
+    full_json = r.json()
+    return [Agency.from_json(d) for d in full_json["agencies"]]
 
 
-def get_chapter_api_response(title: int, chapter: str, date: str) -> requests.Response:
-    raise NotImplementedError
+def get_regulation(reference: CfrReference, date: str) -> Regulation:
+    """
+    Get the XML response for a full title & chapter in ecfr_api_utils
+
+    date is in YYYY-MM-DD format
+    """
+    full_xml_endpoint = f"/api/versioner/v1/full/{date}/title-{reference.title}.xml"
+    url = urljoin(BASE_URL, full_xml_endpoint)
+    params = reference.query_params
+    r = requests.get(url, params=params)
+    return Regulation.from_response(response=r)
--- backend/feature_extraction.py
+++ backend/feature_extraction.py
@@ -1,26 +1,64 @@
 from dataclasses import dataclass
-from .efcr_api_utils import Chapter
+from functools import lru_cache
+from typing import List
+
+import requests
+
+from .ecfr_api_utils import Regulation, Agency, get_regulation
 
 
 class IntFeatureExtractor:
 
-    def extract(self, chapter: Chapter) -> int:
+    def extract(self, regulation: Regulation) -> int:
         raise NotImplementedError
 
 
 class WordCountExtractor(IntFeatureExtractor):
 
     def _get_one_paragraph_word_count(self, paragraph: str) -> int:
-        raise NotImplementedError
+        """
+        Dumb approach of counting words as text w/ spaces, can improve
+        w/ NLP (e.g. simple way would be using spacy)
+        """
+        return len(paragraph.split(" "))
 
-    def extract(self, chapter: Chapter) -> int:
-        return sum(self._get_one_paragraph_word_count(x) for x in chapter.paragraphs)
+    def extract(self, regulation: Regulation) -> int:
+        return sum(self._get_one_paragraph_word_count(x) for x in regulation.paragraphs)
+
+
+class DeiWordCountExtractor(IntFeatureExtractor):
+
+    def extract(self, regulation: Regulation) -> int:
+        raise NotImplementedError
 
 
 @dataclass
-class ChapterFeatures:
+class RegulationFeatures:
     word_count: int
 
     @classmethod
-    def from_chapter(cls, chapter: Chapter) -> "ChapterFeatures":
-        return cls(word_count=WordCountExtractor().extract(chapter=chapter))
+    def from_regulation(cls, regulation: Regulation) -> "RegulationFeatures":
+        return cls(word_count=WordCountExtractor().extract(regulation=regulation))
+
+
+@dataclass
+class AgencyFeatures:
+    word_count: int
+
+    @classmethod
+    def from_regulation_features(
+        cls, regulation_features: List[RegulationFeatures]
+    ) -> "AgencyFeatures":
+        word_count = sum(x.word_count for x in regulation_features)
+        return cls(word_count=word_count)
+
+
+def get_agency_features(agency: Agency, date: str) -> AgencyFeatures:
+    # regulations = []
+    regulation_features = []
+    for cfr_reference in agency.cfr_references:
+        regulation = get_regulation(reference=cfr_reference, date=date)
+        regulation_features.append(
+            RegulationFeatures.from_regulation(regulation=regulation)
+        )
+    return AgencyFeatures.from_regulation_features(regulation_features)
